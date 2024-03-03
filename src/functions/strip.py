e4833d7d8192ab0b4c7a02ff8b39bdf8c87d0ad4
ck <clkerins@gmail.com>
2024-03-03 11:32:28 -0500

    Add hardware.py and refactor strip.py and strip_test.py

--- src/functions/strip.py
+++ src/functions/strip.py
@@ -1,18 +1,7 @@
-from rpi_ws281x import Adafruit_NeoPixel, Color
+from src.functions.hardware import strip, Color
 
 
-# LED strip configuration:
-LED_COUNT      = 32      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 10    # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-
-# Create NeoPixel object with appropriate configuration.
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-
-def turn_off(strip):
+def turn_off():
     # Turn off all LEDs
     for i in range(strip.numPixels()):
         strip.setPixelColor(i, Color(0, 0, 0))
--- tests/strip_test.py
+++ tests/strip_test.py
@@ -1,20 +1,19 @@
 import unittest
 from unittest.mock import MagicMock, call, patch
-from src.functions.strip import turn_off, Color
 
 class TestTurnOff(unittest.TestCase):
     def setUp(self):
         self.strip = MagicMock()
         self.strip.numPixels.return_value = 5
 
-    @patch('src.functions.strip.Adafruit_NeoPixel')
-    @patch('src.functions.strip.Color')
-    def test_turn_off(self, MockColor, MockNeoPixel):
-        MockColor.return_value = (0, 0, 0)
-        turn_off(self.strip)
-        calls = [call(i, MockColor(0, 0, 0)) for i in range(5)]
-        self.strip.setPixelColor.assert_has_calls(calls)
-        self.strip.show.assert_called_once()
+    @patch('src.functions.hardware.strip', new_callable=MagicMock)
+    def test_turn_off(self, mock_strip):
+        mock_strip.numPixels.return_value = 5
+        from src.functions.strip import turn_off
+        turn_off()
+        calls = [call(i, 0) for i in range(5)]
+        mock_strip.setPixelColor.assert_has_calls(calls)
+        mock_strip.show.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
