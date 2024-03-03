c182410b5f2472d990c3a98c5eca76556909365e
ck <clkerins@gmail.com>
2024-03-03 10:59:41 -0500

    Update requirements.txt and add patch in strip_test.py

--- tests/strip_test.py
+++ tests/strip_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, call
+from unittest.mock import MagicMock, call, patch
 from src.functions.strip import turn_off, Color
 
 class TestTurnOff(unittest.TestCase):
@@ -7,9 +7,12 @@ class TestTurnOff(unittest.TestCase):
         self.strip = MagicMock()
         self.strip.numPixels.return_value = 5
 
-    def test_turn_off(self):
+    @patch('src.functions.strip.Adafruit_NeoPixel')
+    @patch('src.functions.strip.Color')
+    def test_turn_off(self, MockColor, MockNeoPixel):
+        MockColor.return_value = (0, 0, 0)
         turn_off(self.strip)
-        calls = [call(i, Color(0, 0, 0)) for i in range(5)]
+        calls = [call(i, MockColor(0, 0, 0)) for i in range(5)]
         self.strip.setPixelColor.assert_has_calls(calls)
         self.strip.show.assert_called_once()
 
